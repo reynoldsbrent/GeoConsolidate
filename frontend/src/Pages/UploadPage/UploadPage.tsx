97e75ba497006eebf9ffb3c09588bea4b2d6ff50
Brent Reynolds <brentsreynolds5@gmail.com>
2025-02-17 22:36:50 -0500

    removed unnecessary comments

--- frontend/src/Pages/UploadPage/UploadPage.tsx
+++ frontend/src/Pages/UploadPage/UploadPage.tsx
@@ -4,7 +4,6 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Fix for default marker icons in React-Leaflet
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
