d245eeb1005032655a954e2cece2ca81230ea5ae
Brent Reynolds <brentsreynolds5@gmail.com>
2025-03-05 18:38:25 -0500

    Added Location model

---
+++ WebAPI/WebAPI/Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class Location
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string name { get; set; } = "";
+    }
+}
